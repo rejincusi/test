ab7acb67a026e255ea30d7ea87df511cd32a5d18
Rejin Cusi <rejincusi@gmail.com>
2019-08-06 12:19:02 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ import 'reflect-metadata'
 import {createKoaServer} from "routing-controllers"
 import Controller from "./controller"
 
-const port = process.env.PORT || 4000
+let port = process.env.PORT || 4000
 
 const app = createKoaServer({
    controllers: [Controller]
 })
-
-console.log('test')
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
